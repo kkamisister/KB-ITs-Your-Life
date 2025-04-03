bfb1c7e3b8042d53492f855af4162115bd4237ba
김윤지 <kkamisister1207@gmail.com>
2025-04-03 14:29:12 +0900

    Docs: 0402 실습파일, 과제평가 업로드

---
+++ assignment/yes24/script.js
@@ -0,0 +1,16 @@
+// script.js
+document.addEventListener('DOMContentLoaded', () => {
+  const tabs = document.querySelectorAll('.tab')
+  const contents = document.querySelectorAll('.tab-content')
+
+  tabs.forEach((tab) => {
+    tab.addEventListener('click', () => {
+      tabs.forEach((t) => t.classList.remove('active'))
+      contents.forEach((c) => c.classList.remove('active'))
+
+      tab.classList.add('active')
+      const target = tab.getAttribute('data-tab')
+      document.getElementById(target).classList.add('active')
+    })
+  })
+})
---
+++ lecture/test/1차_실습과제_18회차_김윤지/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <Calc />
+  </div>
+</template>
+<script>
+// Calc 컴포넌트 import하여 사용
+
+import Calc from './components/Calc.vue'
+// import Calc from './components/Calc2.vue'
+// import Calc from './components/Calc3.vue'
+// import Calc from './components/Calc4.vue'
+// import Calc from './components/Calc5.vue'
+export default {
+  name: 'App',
+  components: { Calc },
+}
+</script>
