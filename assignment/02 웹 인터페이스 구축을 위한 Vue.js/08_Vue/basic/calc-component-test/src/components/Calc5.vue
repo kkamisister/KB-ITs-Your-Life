cbbef6583f70a5c39d0c062d7e00dfefd745b0b9
김윤지 <kkamisister1207@gmail.com>
2025-04-07 05:33:05 +0900

    Docs: 08_Vue 기본문제 위치 이동

---
+++ assignment/02 웹 인터페이스 구축을 위한 Vue.js/08_Vue/basic/calc-component-test/src/components/Calc5.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    X : <input type="text" v-model.number="x" /><br />
+    <div>결과 : {{ result }}</div>
+  </div>
+</template>
+<script>
+import { watch, ref } from 'vue'
+export default {
+  name: 'Calc5',
+  setup() {
+    const x = ref(0)
+    const result = ref(0)
+    watch(x, (cur, old) => {
+      console.log(`${old} => ${cur}`)
+      result.value = cur
+    })
+    return { x, result }
+  },
+}
+</script>
