81593149bd19968dd24d45aceb1a422c143fc921
김윤지 <kkamisister1207@gmail.com>
2025-04-07 05:25:15 +0900

    Docs: 06_Vue 심화문제 위치 이동

---
+++ assignment/02 웹 인터페이스 구축을 위한 Vue.js/06_Vue/advanced/todolist-app/src/components/TodoList.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="row">
+    <div class="col">
+      <ul class="list-group">
+        <TodoListItem
+          v-for="item in todoList"
+          :key="item.id"
+          :item="item"
+          @delete-todo="$emit('delete-todo', $event)"
+          @toggle-completed="$emit('toggle-completed', $event)"
+        />
+      </ul>
+    </div>
+  </div>
+</template>
+<script>
+import TodoListItem from './TodoListItem.vue'
+export default {
+  name: 'TodoList',
+  components: {
+    TodoListItem,
+  },
+  emits: ['delete-todo', 'toggle-completed'],
+  props: {
+    todoList: Array,
+  },
+}
+</script>
