f985117dca4e9e40b6e55ff0aded7e6144286c15
김윤지 <kkamisister1207@gmail.com>
2025-04-07 05:36:01 +0900

    Docs: 09_Vue 기본문제 위치 이동

---
+++ assignment/02 웹 인터페이스 구축을 위한 Vue.js/09_Vue/basic/router-test/src/pages/About.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="card card-body">
+    <h2>About</h2>
+    <p>요청경로 : {{ route.fullPath }}</p>
+  </div>
+</template>
+<script>
+import { useRoute } from 'vue-router'
+export default {
+  name: 'About',
+  setup() {
+    const route = useRoute()
+    return { route }
+  },
+}
+</script>
