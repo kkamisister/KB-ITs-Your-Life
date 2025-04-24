dc18ac7fe41c08e06e8dc5e9e4aa759dac50eddb
김윤지 <kkamisister1207@gmail.com>
2025-04-24 16:35:02 +0900

    Docs: 03_java(기본) 과제 완료

---
+++ assignment/term2/03 백엔드 개발을 위한 자바 프로그래밍/03_java/03_java(기본)/study/src/ch06/sec06/exam01/CarExample.java
@@ -0,0 +1,11 @@
+package ch06.sec06.exam01;
+
+public class CarExample {
+    public static void main(String[] args) {
+        Car myCar = new Car();
+
+        System.out.println("모델명: " + myCar.model);
+        System.out.println("모델명: " + myCar.star);
+        System.out.println("모델명: " + myCar.speed);
+    }
+}
---
+++ assignment/term2/03 백엔드 개발을 위한 자바 프로그래밍/03_java/03_java(기본)/study/src/ch06/sec07/exam01/Car.java
@@ -0,0 +1,10 @@
+package ch06.sec07.exam01;
+
+public class Car {
+    String model;
+    String color;
+    int maxSpeed;
+
+    //생성자 선언
+    Car(String model, String color, int maxSpeed) {}
+}
