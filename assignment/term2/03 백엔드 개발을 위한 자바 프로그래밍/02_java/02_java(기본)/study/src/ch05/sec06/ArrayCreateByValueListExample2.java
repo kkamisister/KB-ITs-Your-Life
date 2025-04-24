36f13fae2cc3e94dc8938023cf424ed99b0c6fed
김윤지 <kkamisister1207@gmail.com>
2025-04-24 16:34:16 +0900

    Docs: 02_java(기본) 과제 완료

--- assignment/term2/03 백엔드 개발을 위한 자바 프로그래밍/02_java/02_java(기본)/study/src/ch05/sec06/ArrayCreateByValueListExample2.java
+++ assignment/term2/03 백엔드 개발을 위한 자바 프로그래밍/02_java/02_java(기본)/study/src/ch05/sec06/ArrayCreateByValueListExample2.java
@@ -2,12 +2,18 @@ package ch05.sec06;
 
 public class ArrayCreateByValueListExample2 {
     public static void main(String[] args) {
-        int[] scores = new int[] { 83, 90, 87 };
-
-        void printItem(int[] scores) {
-            for (int i = 0; i < scores.length; i++) {}
+//        int[] scores = new int[] { 83, 90, 87 };
+        int[] scores;
+        scores = new int[] { 83, 90, 87 };
+        int sum1 = 0;
+        for(int i=0; i<3; i++) {
+            sum1 += scores[i];
         }
-
-        printItem(new int[] scores);
+        printItem( new int[] { 83, 90, 87 } );
+    }
+    public static void printItem( int[] scores ) {
+        for(int i = 0; i<=2; i++) {
+            System.out.println("score[" + i + "]: " + scores[i]);
+        }
     }
 }
