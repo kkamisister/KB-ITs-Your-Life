c25ae8a847d695fa9152ae7e056d71d7b8fc7413
김윤지 <kkamisister1207@gmail.com>
2025-04-27 18:09:41 +0900

    Docs: ch14.exam MovieThread 완료

---
+++ assignment/term2/03 백엔드 개발을 위한 자바 프로그래밍/08_java/08_java(심화)/study/src/ch14/exam/MovieThread.java
@@ -0,0 +1,17 @@
+package ch14.exam;
+
+public class MovieThread {
+    public static void main(String[] args) {
+        Thread thread = new Thread(new Runnable() {
+            public void run() {
+                for (int i = 0; i < 3; i++) {
+                    System.out.println("동영상을 재생합니다.");
+                    try { Thread.sleep(1000); } catch (InterruptedException e) {}
+                }
+            }
+        });
+        thread.start();
+
+
+    }
+}
